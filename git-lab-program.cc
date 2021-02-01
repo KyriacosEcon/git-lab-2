6eb72a1f6825889cd25ac955802aa1def5b79b5e
KyriacosEcon <77705038+KyriacosEcon@users.noreply.github.com>
2021-02-01 17:28:29 -0500

    Add files via upload
    @kristenmasada

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Kyriacos Economides>
+ *        Date: <02/01/2021>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
